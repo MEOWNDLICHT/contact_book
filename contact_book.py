e4a66eaa832116a9895a115ede5b5f402efed1c2
meow <meowndlicht@gmail.com>
2025-07-31 03:02:06 +0800

    improved the code

--- contact_book.py
+++ contact_book.py
@@ -1,20 +1,24 @@
 # CONTACT BOOK PROGRAM
-"""A contact book program that can store and pull data from a json file."""
-
 
 import json
 
 
 class ContactBook:
-
     def __init__(self):
         try:
             with open("contacts.json", "r") as contact_book:
                 self.contacts = json.load(contact_book)
         except json.JSONDecodeError:
-            print("\nThe file contains nothing yet... Please add a person in the contact list.")
-            print("Use the add_contact method, and input the ff values in the parameters: name, phone number, email.\n")
             self.contacts = {}
+        except FileNotFoundError:
+            with open("contacts.json", "r") as contact_book:
+                self.contacts = json.load(contact_book)
+        
+
+    def save_changes(self):
+        with open("contacts.json", "w") as save_data:
+            json.dump(self.contacts, save_data, indent=4)
+
 
 
     def get_contact_list(self):
@@ -25,8 +29,7 @@ class ContactBook:
             print(f"{n}. Name: {name}, Phone number: {info['Phone number']}, Email: {info['Email']}")
         print(f"\nThe total number of people in the Contact Book is: {n}\n")
 
-
-    def search_info(self, name_to_search):
+    def get_contact_info(self, name_to_search: str):
         print(f"\nYou searched for '{name_to_search}':\n")
         n = 0
         
@@ -37,23 +40,22 @@ class ContactBook:
         print(f"\nThe total number of people that matched your search: {n}\n")
 
 
-    def add_contact(self, name, phone_number, email):
+
+    def add_contact(self, name: str, phone_number: str, email: str):
         self.contacts[name] = {
             "Phone number": phone_number,
             "Email" : email
         }
-
-        with open("contacts.json", "w") as save_data:
-            json.dump(self.contacts, save_data)
+        self.save_changes()
         
-        print(f"\nThe contact information of '{name}' has been successfuly added to the Contact Book!\n")
-
-
-    def delete_contact(self, name):
+    def delete_contact(self, name: str):
         if name in self.contacts:
             self.contacts.pop(name)
+        self.save_changes()
 
-        with open("contacts.json", "w") as delete_data:
-            json.dump(self.contacts, delete_data)
-
-        print(f"\nThe contact information of '{name}' has been deleted from the Contact Book.\n")
+    def update_contact(self, name: str, data_to_update: str, new_value: str):
+        if name not in self.contacts:
+            print('Contact not found.')
+        else:
+            self.contacts[name][data_to_update] = new_value
+        self.save_changes()
